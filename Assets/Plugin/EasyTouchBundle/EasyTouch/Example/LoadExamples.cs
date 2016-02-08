880b8591603144db1d128d7a5facf0fe66e5c0ba
tsss-t <s1520728@u.tsukuba.ac.jp>
2016-02-09 04:47:51 +0900

    plugins hide

--- Assets/Plugin/EasyTouchBundle/EasyTouch/Example/LoadExamples.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class LoadExamples : MonoBehaviour {
-
-	public void LoadExample(string level){
-		Application.LoadLevel( level );
-	}
-}
--- Assets/Plugin/EasyTouchBundle/EasyTouch/Example/UnityUI/UICompatibility/UICompatibility.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class UICompatibility : MonoBehaviour {
-
-	public void SetCompatibility(bool value){
-		EasyTouch.SetUICompatibily( value);
-	}
-}
